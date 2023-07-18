55141eadacdfc421b8f84c2e31060ea6fd1ce897
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 21:55:02 +0530

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const require = req("request");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, function () {
+    console.log("server is running on port 3000");
+})
