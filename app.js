ba984560d3bff4cbe238a493b73a959233c17be2
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 22:25:09 +0530

    init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const require = require("request");
+const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
