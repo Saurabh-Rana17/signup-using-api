272d49ee50643229469da4e9e85d0215ac9bc0c3
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 23:30:26 +0530

    added static public folder

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(3000, function () {
     console.log("server is running on port 3000");
+})
+
+app.get("/", function (req,res) {
+    res.sendFile(__dirname + "/success.html");
 })
