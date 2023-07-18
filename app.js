accea9286c3e360177340e2eb0238d0da792f703
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-18 22:20:58 +0530

    refactor

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const require = req("request");
+const require = require("request");
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(3000, function () {
     console.log("server is running on port 3000");
 })
